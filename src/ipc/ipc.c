d1e5472327a33c04ca9767b179b77d090d690dbc
Kevin James <KevinJames@thekev.in>
2015-02-12 11:41:12 -0500

    ensure irq is enabled and disabled properly in ipc functions

--- src/ipc/ipc.c
+++ src/ipc/ipc.c
@@ -7,20 +7,25 @@
 int k_send_message(int destination_proc_id, void *message_envelope) {
 		int sender_proc_id;
 		PCB *receiving_proc;
+
 		__disable_irq();
-	
+
 		sender_proc_id = gp_current_process->pid;
 		receiving_proc = k_get_pcb_from_pid(destination_proc_id);
 		list_push(&receiving_proc->msg_queue, message_envelope);
-		if ( receiving_proc->state == BLOCKED_ON_RECEIVE ) {
+
+		if (receiving_proc->state == BLOCKED_ON_RECEIVE) {
 				receiving_proc->state = READY;
 				k_dequeue_process(destination_proc_id);
 				k_enqueue_process(destination_proc_id);
-				
-				if(k_get_proc_table_from_pid(destination_proc_id)->m_priority > k_get_proc_table_from_pid(sender_proc_id)->m_priority){
+
+				if (k_get_proc_table_from_pid(destination_proc_id)->m_priority > k_get_proc_table_from_pid(sender_proc_id)->m_priority){
+						__enable_irq();
 						k_release_processor();
+						__disable_irq();
 				}
 		}
+
 		__enable_irq();
 
 		return RTX_OK;
@@ -28,17 +33,16 @@ int k_send_message(int destination_proc_id, void *message_envelope) {
 
 void *k_receive_message(int *sender_id) {//blocks
 		void *env;
-	
-		__disable_irq();
-	
-		while ( list_empty(&gp_current_process->msg_queue) ) {
-				gp_current_process->state = BLOCKED_ON_RECEIVE ;
-				k_release_processor ( ) ;
+
+		while (list_empty(&gp_current_process->msg_queue)) {
+				gp_current_process->state = BLOCKED_ON_RECEIVE;
+				k_release_processor();
 		}
+
+		__disable_irq();
 		env = list_front(&gp_current_process->msg_queue);
 		list_shift(&gp_current_process->msg_queue);
-		
 		__enable_irq();
-		
-		return env ;
+
+		return env;
 }
