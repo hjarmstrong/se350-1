b77010ad0e38d418cfc302d4bb5b2f56dd3e922a
Laura Stericker <lsterick@uwaterloo.ca>
2015-03-19 13:31:26 -0400

    Move string helper functions to string.c

--- src/util/string.c
+++ src/util/string.c
@@ -54,3 +54,32 @@ void *hash_string(const char* str, int len) {
 
     return (void *) hash;
 }
+
+void int_to_c_string(int input, char *output){//works for numbers < 999
+    int index = 0;//current position in the string
+    if(input >= 100){
+        output[index] = input / 100;
+        index++;
+        input %= 100;
+    }
+    if(input >= 10 || index == 1){//If there was a hundreds digit, we can't skip the tens even if it is 0
+        output[index] = input / 10;
+        index++;
+        input %= 10;
+    }
+    output[index] = input;
+    output[index + 1] = '\0';
+}
+
+int c_string_to_int(char *input){//input must be NULL terminated
+    int char_offset = 48;
+    int index;//current position in the string
+    int multiplier = 1;
+    int output;
+    for(index = 0; input[index] != '\0'; index++){}//index will be one past the end of the string
+    index--;//last character
+    for(;index >= 0; index--, multiplier *= 10){
+        output += (input[index] - char_offset) * multiplier;
+    }
+    return output;
+}
--- src/util/string.h
+++ src/util/string.h
@@ -5,5 +5,7 @@ void strncpy(char *destination, const char *source, int n);
 int strlen(const char *str);
 int strncmp(const char *a, const char *b, int n);
 void *hash_string(const char* str, int len);
+void int_to_c_string(int input, char *output);
+int c_string_to_int(char *input);
 
 #endif // STRING_H
